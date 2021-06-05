ff3d1a0407b6a292080b214de79577ab2257142e
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-06-05 19:33:38 -0400

    set up handlers and states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React, { useState } from "react";
 import "./App.css";
+//Components
+import Form from "./components/Form";
+import ToDoList from "./components/ToDoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <header>
+        <h1>Hello React!</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <ToDoList />
     </div>
   );
 }

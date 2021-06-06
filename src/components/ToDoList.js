82d99bbef8204afbe25674a628c032c9b9566712
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-06-05 21:52:38 -0400

    set props and states correctly, deleted function onclick works as it should

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,15 +1,20 @@
 import React from "react";
 
-import Todo from './ToDo';
+import Todo from "./ToDo";
 
-const ToDoList = ({todos}) => {
-
+const ToDoList = ({ todos, setTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-          {todos.map(todo => (
-              <Todo key ={todo.id} text={todo.text} id={todo.id} />
-          ))}
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+            />
+        ))}
       </ul>
     </div>
   );
